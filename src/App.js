f2d06987dbfe3934d6bf9e9d58cfcac9802d5616
maeve <mujammilkhan321@gmail.com>
2020-08-22 14:37:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Provider } from "react-redux";
-import store from "./redux/store";
+
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Create from "./pages/Create";
@@ -8,26 +7,21 @@ import Disliked from "./pages/Disliked";
 import Liked from "./pages/Liked";
 import Post from "./pages/Post";
 import PostUpdate from "./pages/PostUpdate";
+import Search from "./pages/Search";
 
 function App() {
     return (
-        <Provider store={store}>
-            <Router>
-                <Switch>
-                    <Route path="/" component={Home} exact />
-                    <Route path="/liked" component={Liked} exact />
-                    <Route path="/disliked" component={Disliked} exact />
-                    <Route path="/posts/create" component={Create} exact />
-                    <Route path="/posts/:id" component={Post} exact />
-                    <Route
-                        path="/posts/:id/update"
-                        component={PostUpdate}
-                        exact
-                    />
-                    <Route path="/search" component={Home} />
-                </Switch>
-            </Router>
-        </Provider>
+        <Router>
+            <Switch>
+                <Route path="/" component={Home} exact />
+                <Route path="/liked" component={Liked} exact />
+                <Route path="/disliked" component={Disliked} exact />
+                <Route path="/posts/create" component={Create} exact />
+                <Route path="/posts/:id" component={Post} exact />
+                <Route path="/posts/:id/update" component={PostUpdate} exact />
+                <Route path="/search" component={Search} />
+            </Switch>
+        </Router>
     );
 }
 
